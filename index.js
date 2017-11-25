3d79f7b371f3d53ed13c2a574ff70c0f49fdf9f4
Tuomas Änäkkälä <tuomas.anakkala@icloud.com>
2017-11-25 23:10:53 +0200

    fetch bearer token

--- index.js
+++ index.js
@@ -2,20 +2,52 @@ var express = require('express');
 var app = express();
 var env = require('node-env-file');
 var requestProxy = require('express-request-proxy');
+var request = require('request');
 
 if(process.env.NODE_ENV !== "production") {
     env(__dirname + '/.env');
 }
 
-app.get('/api/:resource/:id', requestProxy({
-    url: "https://someapi.com/api/:resource/:id",
-    query: {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET
-    },
-    headers: {
-        'X-Custom-Header': process.env.SOMEAPI_CUSTOM_HEADER
-    }
-}));
+const BASE64_ENC = process.env.BASE64_ENC;
+const PORT = process.env.PORT;
 
-console.log('running');
+var proxyObject = {
+    url: "https://api.spotify.com/v1/:resource/:id",
+    headers: {
+        'Authorization': 'Bearer ' + token
+    },
+    json: true
+};
+
+/* start listening */
+app.listen(PORT, function () {
+    console.log('listening');
+    getToken();
+});
+
+app.get('/', function(req, res) {
+    res.send("hello world");
+});
+
+/* options to obtain bearer */
+var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic ' + BASE64_ENC
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+};
+
+function getToken() {
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            proxyObject.headers.Authorization = 'Bearer ' + body.access_token;
+            setTimeout(getToken, 1800000);
+        }
+    });
+}
+
+app.get('/api/:resource/:id', requestProxy(proxyObject));
